d1a0b89395fce6c44d021a4fe29314d2ee0a8f56
DESKTOP-EO79GOM\ekusn <ekusnarenko26@gmail.com>
2026-07-25 18:09:07 +0700

    frontend

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 ﻿from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from src.database import init_db
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
@@ -9,9 +12,19 @@ async def lifespan(app: FastAPI):
     await init_db()
     yield
 
-app = FastAPI(title="File Analyzer Service", lifespan=lifespan)
 
 BASE_DIR = Path(__file__).resolve().parent
-
-# Подключаем папку static, которая находится внутри src/
+app = FastAPI(title="File Analyzer Service", lifespan=lifespan)
+templates = Jinja2Templates(directory=BASE_DIR /"templates")
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="base.html")
+@app.get("/download", response_class=HTMLResponse)
+async def download_page(request: Request):
+    return templates.TemplateResponse(request=request, name="download.html")
+
+@app.get("/files", response_class=HTMLResponse)
+async def download_page(request: Request):
+    return templates.TemplateResponse(request=request, name="files.html")
